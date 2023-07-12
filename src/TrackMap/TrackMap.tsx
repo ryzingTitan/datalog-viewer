745848fc35ded8f65d380cbdb741165bd438cb50
ryzingTitan <computerwhiz33@gmail.com>
2023-07-12 18:35:49 -0400

    Added play, pause, and restart buttons for track map screen

--- src/TrackMap/TrackMap.tsx
+++ src/TrackMap/TrackMap.tsx
@@ -6,14 +6,18 @@ import TrackMapMarker from "./TrackMapMarker";
 import DataGauges from "../DataGauges/DataGauges";
 import { useLoaderData } from "react-router-dom";
 import Datalog from "../Session/Datalog";
+import TrackMapButtons from "../TrackMapButtons/TrackMapButtons";
+import { Stack } from "@mui/material";
 
 export default function TrackMap(): ReactElement {
   const datalogs: Datalog[] = useLoaderData() as Datalog[];
 
   const [currentIndex, setCurrentIndex] = useState(0);
   const [previousIndex, setPreviousIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [playSpeed, setPlaySpeed] = useState(1);
 
-  const intervalTime = 500;
+  const intervalTime = 1000;
 
   const coordinates = Array<[number, number]>();
 
@@ -22,39 +26,49 @@ export default function TrackMap(): ReactElement {
   });
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (currentIndex >= coordinates.length - 1) {
-        setCurrentIndex(0);
-        setPreviousIndex(0);
-        return;
-      }
+    if (isPlaying) {
+      const interval = setInterval(() => {
+        if (currentIndex >= coordinates.length - 1) {
+          setCurrentIndex(0);
+          setPreviousIndex(0);
+          return;
+        }
 
-      setPreviousIndex(currentIndex);
-      setCurrentIndex(currentIndex + 1);
-    }, intervalTime);
+        setPreviousIndex(currentIndex);
+        setCurrentIndex(currentIndex + 1);
+      }, intervalTime / playSpeed);
 
-    return () => clearInterval(interval);
-  }, [coordinates.length, currentIndex]);
+      return () => clearInterval(interval);
+    }
+  }, [coordinates.length, currentIndex, isPlaying, playSpeed]);
 
   return (
     <Box sx={{ paddingTop: 2 }}>
-      <MapContainer
-        style={{ height: 420 }}
-        center={[42.4086, -86.1374]}
-        zoom={16}
-        scrollWheelZoom={false}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <TrackMapMarker
-          coordinates={coordinates}
-          previousIndex={previousIndex}
-          currentIndex={currentIndex}
-          intervalTime={intervalTime}
-        />
-      </MapContainer>
+      <Stack direction={"row"} justifyContent={"center"} alignItems={"center"}>
+        <MapContainer
+          style={{ height: 420, width: 900 }}
+          center={[42.4086, -86.1374]}
+          zoom={16}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <TrackMapMarker
+            coordinates={coordinates}
+            previousIndex={previousIndex}
+            currentIndex={currentIndex}
+            intervalTime={intervalTime / playSpeed}
+          />
+        </MapContainer>
+        <TrackMapButtons
+          setIsPlaying={setIsPlaying}
+          setPlaySpeed={setPlaySpeed}
+          playSpeed={playSpeed}
+          setCurrentIndex={setCurrentIndex}
+        ></TrackMapButtons>
+      </Stack>
       <DataGauges datalogs={datalogs} currentIndex={currentIndex}></DataGauges>
     </Box>
   );
---
+++ src/TrackMapButtons/TrackMapButtonsProps.ts
@@ -0,0 +1,6 @@
+export default interface TrackMapButtonsProps {
+  setIsPlaying: Function;
+  setPlaySpeed: Function;
+  setCurrentIndex: Function;
+  playSpeed: number;
+}
