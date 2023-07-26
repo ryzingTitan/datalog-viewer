21a4adc73170466918689ac5e2c38bc665180fea
ryzingTitan <computerwhiz33@gmail.com>
2023-07-26 01:27:45 -0400

    Switching to MUI charts

--- src/TemperatureGraph/TemperatureGraph.tsx
+++ src/TemperatureGraph/TemperatureGraph.tsx
@@ -1,61 +1,60 @@
 import { ReactElement } from "react";
-import ReactECharts from "echarts-for-react";
-import { Box } from "@mui/system";
 import { format, parseISO } from "date-fns";
 import { useLoaderData } from "react-router-dom";
 import Datalog from "../Session/Datalog";
+import { LineChart } from "@mui/x-charts";
 
 export default function TemperatureGraph(): ReactElement {
   const datalogs: Datalog[] = useLoaderData() as Datalog[];
 
-  const labels = Array<string>();
+  const labels = Array<Date>();
   const intakeTemperatureDataPoints = Array<number>();
   const coolantTemperatureDataPoints = Array<number>();
   datalogs.forEach((datalog) => {
-    labels.push(format(parseISO(datalog.timestamp), "h:mm:ss a"));
+    labels.push(parseISO(datalog.timestamp));
     intakeTemperatureDataPoints.push(datalog.data.intakeAirTemperature);
     coolantTemperatureDataPoints.push(datalog.data.coolantTemperature);
   });
 
-  const option = {
-    animationDuration: 10000,
-    title: {
-      text: "Temperatures",
-      left: "center",
-    },
-    xAxis: {
-      data: labels,
-      name: "Timestamp",
-      nameLocation: "center",
-      nameGap: 30,
-    },
-    yAxis: {
-      min: "dataMin",
-      axisLabel: {
-        formatter: "{value} \u2109",
-      },
-    },
-    series: [
-      {
-        data: intakeTemperatureDataPoints,
-        type: "line",
-        name: "Intake Air Temperature",
-      },
-      {
-        data: coolantTemperatureDataPoints,
-        type: "line",
-        name: "Coolant Temperature",
-      },
-    ],
-    tooltip: {
-      trigger: "axis",
-      valueFormatter: (value: string) => `${value} \u2109`,
-    },
-  };
-
   return (
-    <Box sx={{ paddingTop: 2 }}>
-      <ReactECharts option={option}></ReactECharts>
-    </Box>
+    <LineChart
+      sx={{
+        "& .MuiMarkElement-root": {
+          display: "none",
+        },
+      }}
+      legend={{ direction: "column" }}
+      xAxis={[
+        {
+          data: labels,
+          label: "Timestamp",
+          scaleType: "time",
+          tickMinStep: 300000,
+          valueFormatter: (value: Date) => format(value, "h:mm a"),
+        },
+      ]}
+      yAxis={[
+        {
+          valueFormatter: (value: string) => `${value} \u2109`,
+          min: 100,
+        },
+      ]}
+      series={[
+        {
+          data: intakeTemperatureDataPoints,
+          label: "Intake Air Temperature",
+          valueFormatter: (value: number) => `${value} \u2109`,
+        },
+        {
+          data: coolantTemperatureDataPoints,
+          label: "Coolant Temperature",
+          valueFormatter: (value: number) => `${value} \u2109`,
+        },
+      ]}
+      tooltip={{
+        trigger: "axis",
+      }}
+      height={400}
+    />
   );
 }
--- src/ThrottleGraph/ThrottleGraph.tsx
+++ src/ThrottleGraph/ThrottleGraph.tsx
@@ -1,94 +1,68 @@
 import { ReactElement } from "react";
-import ReactECharts from "echarts-for-react";
-import * as echarts from "echarts/core";
-import { LinesChart } from "echarts/charts";
-import {
-  GridComponent,
-  ToolboxComponent,
-  TooltipComponent,
-  TitleComponent,
-} from "echarts/components";
-import { CanvasRenderer } from "echarts/renderers";
-import { Box } from "@mui/system";
 import { format, parseISO } from "date-fns";
 import { useLoaderData } from "react-router-dom";
 import Datalog from "../Session/Datalog";
+import { LineChart } from "@mui/x-charts";
 
 export default function ThrottleGraph(): ReactElement {
   const datalogs: Datalog[] = useLoaderData() as Datalog[];
 
-  echarts.use([
-    TitleComponent,
-    GridComponent,
-    CanvasRenderer,
-    LinesChart,
-    ToolboxComponent,
-    TooltipComponent,
-  ]);
-
-  const labels = Array<string>();
+  const labels = Array<Date>();
   const engineRpmDataPoints = Array<number>();
   const throttlePositionDataPoints = Array<number>();
   datalogs.forEach((datalog) => {
-    labels.push(format(parseISO(datalog.timestamp), "h:mm:ss a"));
+    labels.push(parseISO(datalog.timestamp));
     engineRpmDataPoints.push(datalog.data.engineRpm);
     throttlePositionDataPoints.push(datalog.data.throttlePosition);
   });
 
-  const option = {
-    animationDuration: 10000,
-    title: {
-      text: "Throttle Data",
-      left: "center",
-    },
-    xAxis: {
-      data: labels,
-      name: "Timestamp",
-      nameLocation: "center",
-      nameGap: 30,
-    },
-    yAxis: [
-      {
-        min: "dataMin",
-        axisLabel: {
-          formatter: "{value} RPM",
-        },
-      },
-      {
-        name: "Throttle Position",
-        axisLabel: {
-          formatter: "{value} %",
-        },
-      },
-    ],
-    series: [
-      {
-        data: engineRpmDataPoints,
-        type: "line",
-        name: "Engine RPM",
-        yAxisIndex: 0,
-        tooltip: {
-          valueFormatter: (value: string) => value + " RPM",
-        },
-      },
-      {
-        data: throttlePositionDataPoints,
-        type: "bar",
-        name: "Throttle Position",
-        yAxisIndex: 1,
-        tooltip: {
-          valueFormatter: (value: string) => value + " %",
-        },
-      },
-    ],
-    tooltip: {
-      trigger: "axis",
-    },
-  };
-
   return (
-    <Box sx={{ paddingTop: 2 }}>
-      <ReactECharts option={option}></ReactECharts>
-    </Box>
+    <LineChart
+      sx={{
+        "& .MuiMarkElement-root": {
+          display: "none",
+        },
+      }}
+      legend={{ direction: "column" }}
+      xAxis={[
+        {
+          data: labels,
+          label: "Timestamp",
+          scaleType: "time",
+          tickMinStep: 300000,
+          valueFormatter: (value: Date) => format(value, "h:mm a"),
+        },
+      ]}
+      yAxis={[
+        {
+          id: "engineRpm",
+          valueFormatter: (value: string) => `${value} RPM`,
+        },
+        {
+          id: "throttlePosition",
+          valueFormatter: (value: string) => `${value} %`,
+        },
+      ]}
+      series={[
+        {
+          data: engineRpmDataPoints,
+          label: "Engine RPM",
+          valueFormatter: (value: number) => `${value} RPM`,
+          yAxisKey: "engineRpm",
+        },
+        {
+          data: throttlePositionDataPoints,
+          label: "Throttle Position",
+          valueFormatter: (value: number) => `${value} %`,
+          yAxisKey: "throttlePosition",
+        },
+      ]}
+      tooltip={{
+        trigger: "axis",
+      }}
+      height={400}
+      margin={{ left: 70 }}
+      rightAxis="throttlePosition"
+    />
   );
 }
